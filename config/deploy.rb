e1debf43c26502aba5998c55a13a79d96a6d076c
hideack <hide.terai@gmail.com>
2014-02-22 17:56:04 +0900

    sharedからnode_modulesを外す

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :branch, 'master'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['log', 'node_modules']
+set :shared_paths, ['log']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
@@ -77,9 +77,9 @@ end
 # all releases.
 task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/log"]
-  queue! %[mkdir -p "#{deploy_to}/shared/node_modules"]
+  #queue! %[mkdir -p "#{deploy_to}/shared/node_modules"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/node_modules"]
+  #queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/node_modules"]
 end
 
 desc "Deploys the current version to the server."
@@ -89,10 +89,10 @@ task :deploy => :environment do
     # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
-    invoke :'rendr:install'
-    invoke :'rendr:compile'
 
     to :launch do
+      invoke :'rendr:install'
+      invoke :'rendr:compile'
       invoke :'rendr:stop'
       invoke :'rendr:start'
       queue "touch #{deploy_to}/tmp/restart.txt"
