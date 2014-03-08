f74912fdd714bcdd8414eebde335426fa385da89
hikarock <hikari@rockf.es>
2014-03-08 23:28:54 +0900

    postRenderの処理を関数に分割した

--- app/views/editor/index.js
+++ app/views/editor/index.js
@@ -2,119 +2,33 @@ var BaseView = require('../base'),
     _        = require('underscore'),
     Code     = require('../../models/code');
 
-
 module.exports = BaseView.extend({
   className: 'editor_index_view',
 
-  postInitialize: function() {
-    //
-  },
+  //TODO: 固定ではなくパラメータからsetするように変える
+  fileName: 'bar.js',
+  unique:   '6hck4e',
+  token:    'y0zdzw2xs6n42pj97aby6zugdbmwnyttcjphobhh',
+  body:     '',
 
-  //TODO:
-  unique: '6hck4e',
-  token:  'y0zdzw2xs6n42pj97aby6zugdbmwnyttcjphobhh',
-  body:   '',
-
+  /*
+   * コードを保存する
+   */
   update: function(evt) {
     console.log('update');
-    var self = this;
+    var that = this;
     var code = new Code({
       'unique': this.unique,
       'token':  this.token,
       'body':   this.body
       //TODO: fileNameも保存したい
     });
-
     code.save();
   },
 
-  //TODO:
-  fileName: 'bar.js',
-
-  postRender: function() {
-    console.log("editor view postRender");
-
-    var self = this;
-    $.event.props.push("dataTransfer");
-
-    var editor = ace.edit("editor");
-    editor.setReadOnly(true);
-    editor.setPrintMarginColumn(false);
-    editor.setTheme("ace/theme/ambiance");
-
-    $('#editor').on('drop', function(evt) {
-      evt.preventDefault();
-      $(this).removeClass('over');
-      var files = evt.dataTransfer.files;
-      var f = files[0];
-
-      //TODO:
-      if (self.getExtention(f.name) == 'js') {
-        editor.getSession().setMode("ace/mode/javascript");
-      }
-
-      var reader = new FileReader();
-
-      if (!f.type.match('text.*')) {
-        alert('Only text file.');
-        return;
-      }
-
-      self.fileName = f.name;
-
-      lastMod = f.lastModifiedDate;
-
-      setInterval(function() {
-        tick(f);
-      }, 250);
-
-      reader.onload = function(evt) {
-        self.body = reader.result;
-        editor.setValue(self.body);
-        editor.clearSelection();
-      };
-
-      reader.readAsText(f);
-    });
-
-    $('#editor').on('dragleave', function(evt) {
-      $(this).removeClass('over');
-      evt.preventDefault();
-    });
-
-    $('#editor').on('dragover', function(evt) {
-      $(this).addClass('over');
-      evt.preventDefault();
-    });
-
-    var lastMod;
-    var tick = function(f) {
-      if (f && f.lastModifiedDate.getTime() != lastMod.getTime()) {
-        lastMod = f.lastModifiedDate;
-        console.log(f.lastModifiedDate + ":" + lastMod);
-        var reader = new FileReader();
-        reader.onload = function (evt) {
-          self.body = reader.result;
-          editor.setValue(self.body);
-          editor.clearSelection();
-          self.update();
-        };
-        reader.readAsText(f);
-      }
-    };
-  },
-
-  getTemplateData: function() {
-
-    var data = BaseView.prototype.getTemplateData.call(this);
-
-    data = _.extend({}, data, {
-      code: this.options.code.toJSON()
-    });
-
-    return data;
-  },
-
+  /*
+   * ファイル名の拡張子を取得する
+   */
   getExtention: function(fileName) {
     var ret;
     if (!fileName) {
@@ -127,10 +41,122 @@ module.exports = BaseView.extend({
     }
     ret = fileTypes[len - 1];
     return ret;
-  }
+  },
+
+  handleDragleave: function(evt) {
+    $(this).removeClass('over');
+  },
+
+  handleDragover: function(evt) {
+    $(this).addClass('over');
+  },
+
+  handleDrop: function(evt) {
+    var that = this;
+    $(this).removeClass('over');
+    var files = evt.dataTransfer.files;
+    var f = files[0];
+
+    //TODO: js以外も判定する
+    if (that.getExtention(f.name) == 'js') {
+      that.editor.getSession().setMode("ace/mode/javascript");
+    }
+
+    that.reader = new FileReader();
+
+    if (!f.type.match('text.*')) {
+      alert('Only text file.');
+      return;
+    }
+
+    that.fileName = f.name;
+    $('#file-name').text(f.name);
+
+    that.lastMod = f.lastModifiedDate;
+
+    setInterval(function() {
+      that.tick(f);
+    }, 250);
+
+    that.reader.onload = function(evt) {
+      that.handleLoadReader(evt);
+    };
+
+    that.reader.readAsText(f);
+  },
+
+  handleLoadReader: function(evt) {
+    var that = this;
+    that.body = that.reader.result;
+    that.editor.setValue(that.body);
+    that.editor.clearSelection();
+    that.update();
+  },
+
+  setEditor: function() {
+    var that = this,
+        theme = 'ace/theme/ambiance',
+        defaultMode = 'ace/mode/javascript';
+
+    that.editor = ace.edit("editor");
+    that.editor.setReadOnly(true);
+    that.editor.setPrintMarginColumn(false);
+    that.editor.setTheme(theme);
+    that.editor.getSession().setMode(defaultMode);
+  },
+
+  lastMod: '',
+
+  tick: function(f) {
+    var that = this;
+    if (f && f.lastModifiedDate.getTime() != that.lastMod.getTime()) {
+      that.lastMod = f.lastModifiedDate;
+      handleLoadReader();
+      that.reader.readAsText(f);
+    }
+  },
+
+  /*
+   * Environment: client.
+   */
+  postRender: function() {
+    var that = this;
+    $.event.props.push("dataTransfer");
+    that.setEditor();
+    $editor = $('#editor');
+
+    $editor.on('drop', function(evt) {
+      evt.preventDefault();
+      that.handleDrop(evt);
+    });
+
+    $editor.on('dragleave', function(evt) {
+      evt.preventDefault();
+      that.handleDragleave(evt);
+    });
+
+    $editor.on('dragover', function(evt) {
+      evt.preventDefault();
+      that.handleDragover(evt);
+    });
+  },
+
+  /*
+   * Environment: shared.
+   */
+  getTemplateData: function() {
+
+    var data = BaseView.prototype.getTemplateData.call(this);
+
+    data = _.extend({}, data, {
+      code: this.options.code.toJSON()
+    });
+
+    return data;
+  },
+
 });
 
 module.exports.id = 'editor/index';
 console.log("editor view");
-
 
