58fcf85290f97f06040a729725b195a9646cb1ff
hikarock <hikari@rockf.es>
2014-03-26 00:52:25 +0900

    トップページのデザイン調整

--- app/views/home/index.js
+++ app/views/home/index.js
@@ -3,6 +3,9 @@ var BaseView = require('../base');
 module.exports = BaseView.extend({
   className: 'home_index_view',
   postRender: function() {
+    for (var i = 0; i < 100; i++) {
+      $('#line-number').append($('<li>'));
+    }
     $('#start').on('click', function(evt) {
       evt.preventDefault();
       location.href = $(this).attr('href');
