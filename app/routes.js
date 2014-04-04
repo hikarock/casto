b1a9a607ce2a1adc4c65cd9fe3a067e5c07127f3
hideack <hide.terai@gmail.com>
2014-04-04 21:32:22 +0900

    エラールーティング追加

--- app/routes.js
+++ app/routes.js
@@ -2,4 +2,5 @@ module.exports = function(match) {
   match('',          'home#index');
   match('a',         'token#index');
   match('a/:unique', 'code#index');
+  match('*',         'error#404');
 };
