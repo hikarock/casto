fab63d4d36ee50651c4eb12b4e49f19b43a4b8c0
hikarock <i@hika69.com>
2014-04-06 20:48:26 +0900

    エラーページのマークアップ

--- app/views/error/404.js
+++ app/views/error/404.js
@@ -2,7 +2,20 @@ var BaseView = require('../base');
 
 module.exports = BaseView.extend({
   className: 'error_404_view',
+  setEditor: function() {
+    var theme = 'ace/theme/tomorrow_night_eighties',
+        mode  = 'ace/mode/javascript',
+        editor;
+
+    editor = ace.edit('editor');
+    editor.setReadOnly(true);
+    editor.setPrintMarginColumn(false);
+    editor.setTheme(theme);
+    editor.setSelectionStyle('line');
+    editor.getSession().setMode(mode);
+  },
   postRender: function() {
+    this.setEditor();
   }
 });
 module.exports.id = 'error/404';
